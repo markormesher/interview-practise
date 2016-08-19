64d0382bd6d4696f64a3d274a5508c6f888b21ad
Mark Ormesher <me@markormesher.co.uk>
2016-08-19 16:32:13 +0100

    001: check if all chars in a string are uniques

--- 001-check-all-unique-chars/CheckAllUniqueChars.java
+++ 001-check-all-unique-chars/CheckAllUniqueChars.java
@@ -1,13 +1,13 @@
 import java.util.*;
 
-public class AllUniqueChars {
+public class CheckAllUniqueChars {
 
 	/*
 	O(n) time
 	O(n) space, or O(c) if the character range is known
 	could also be implemented with an array of booleans, if the character range is known
 	*/
-	private static boolean allUniqueCharsWithHashSet(String str) {
+	private static boolean checkAllUniqueCharsWithHashSet(String str) {
 		HashSet<Character> set = new HashSet<>();
 
 		for (int i = 0; i < str.length(); ++i) {
@@ -23,7 +23,7 @@ public class AllUniqueChars {
 	O(1) extra space
 	lowercase a-z only
 	*/
-	private static boolean allUniqueCharsWithBitVector(String str) {
+	private static boolean checkAllUniqueCharsWithBitVector(String str) {
 		int bitVector = 0;
 
 		for (int i = 0; i < str.length(); ++i) {
@@ -47,7 +47,7 @@ public class AllUniqueChars {
 	O(n^2) time
 	O(1) extra space
 	*/
-	private static boolean allUniqueCharsWithTraversal(String str) {
+	private static boolean checkAllUniqueCharsWithTraversal(String str) {
 		for (int i = 0; i < str.length() - 1; ++i) {
 			char compareTo = str.charAt(i);
 			for (int j = i + 1; j < str.length(); ++j) {
@@ -61,13 +61,13 @@ public class AllUniqueChars {
 	public static void main(String... args) {
 		if (args.length == 1) {
 			System.out.println("Using a HashSet..");
-			System.out.println(allUniqueCharsWithHashSet(args[0]) ? "All unique!" : "Contains duplicates");
+			System.out.println(checkAllUniqueCharsWithHashSet(args[0]) ? "All unique!" : "Contains duplicates");
 
 			System.out.println("\nUsing a bit vector...");
-			System.out.println(allUniqueCharsWithBitVector(args[0]) ? "All unique!" : "Contains duplicates");
+			System.out.println(checkAllUniqueCharsWithBitVector(args[0]) ? "All unique!" : "Contains duplicates");
 
 			System.out.println("\nUsing traversal...");
-			System.out.println(allUniqueCharsWithTraversal(args[0]) ? "All unique!" : "Contains duplicates");
+			System.out.println(checkAllUniqueCharsWithTraversal(args[0]) ? "All unique!" : "Contains duplicates");
 		} else {
 			System.err.println("You must provide a single string arugment");
 		}
